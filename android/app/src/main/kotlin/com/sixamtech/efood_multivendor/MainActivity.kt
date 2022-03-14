090c4fbaf26f684854b69172e2fc697486876e25
phouvong <48210806+phouvong@users.noreply.github.com>
2022-03-15 00:16:12 +0700

    ອັພຂື້ນຄັ້ງແລກ
    ອັພຂື້ນຄັ້ງແລກ

---
+++ android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.unilaos.fuser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/styles.dart
@@ -0,0 +1,26 @@
+import 'package:efood_multivendor/util/dimensions.dart';
+import 'package:flutter/material.dart';
+
+final robotoRegular = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w400,
+  fontSize: Dimensions.fontSizeDefault,
+);
+
+final robotoMedium = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w500,
+  fontSize: Dimensions.fontSizeDefault,
+);
+
+final robotoBold = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w700,
+  fontSize: Dimensions.fontSizeDefault,
+);
+
+final robotoBlack = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w900,
+  fontSize: Dimensions.fontSizeDefault,
+);
